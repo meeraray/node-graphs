436cdf3e05eda08aeabcaed155830ef355f8a546
Ronuk Ray <ronukray@gmail.com>
2019-07-03 13:45:09 -0700

    Set up basic UI structure
    Not functional yet, but set up UI with tool options, hovering and selecting

---
+++ scripts.js
@@ -0,0 +1,20 @@
+// AESTHETICS
+var SELECTED_COLOR = "#f3a5f3";
+
+
+var nodes = [];
+var seen = [];
+
+var selected = 0;
+//const NEW_NODE = 1; 
+
+$(function() {
+	console.log("ready");
+    
+    $(".control").click(function() {
+        $(".control").removeClass("selected");
+        $(this).addClass("selected");
+    });
+    
+    
+});	
