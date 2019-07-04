52a8b2819f288fe0389f9ec488407526daf8949b
Ronuk Ray <ronukray@gmail.com>
2019-07-03 18:08:47 -0700

    Got first button (add nodes) working
    figured out how to map mouse clicks to node coordinates, place them

--- scripts.js
+++ scripts.js
@@ -1,20 +1,63 @@
-// AESTHETICS
-var SELECTED_COLOR = "#f3a5f3";
-
+var CIRCLE_WIDTH = 30;
 
 var nodes = [];
 var seen = [];
+var messages = [
+    "Click to place node.",
+    "Click on node to delete.",
+    "Click on node, then on other node to connect them.",
+    "Click on line to delete.",
+    "Click and drag node to move it."
+];
 
-var selected = 0;
-//const NEW_NODE = 1; 
+var selected = -1;
+const NEW_NODE = 0; 
+const DELETE_NODE = 1;
+const NEW_LINE = 2;
+const DELETE_LINE = 3;
+const MOVE_NODE = 4;
 
 $(function() {
 	console.log("ready");
     
     $(".control").click(function() {
+        var index = $(".control").index($(this));
+        console.log(index);
+        
         $(".control").removeClass("selected");
         $(this).addClass("selected");
+        
+        $("#message").html(messages[index]);
+        
+        selected = index;
     });
     
+    $("#stage").click(function(e) {  
+        var pos = $(this).offset();
+        //console.log(e.pageX + ", " + e.pageY);
+        //console.log(pos.left + ", " + pos.top);
+        if(selected == NEW_NODE) {
+            handleAddNode(e.pageX - pos.left, e.pageY - pos.top)
+        }
+        else if (selected == DELETE_NODE) {
+            
+        }
+    });
     
+    function handleAddNode(xCoord, yCoord) {
+        console.log(xCoord, yCoord);
+        var node = {x: xCoord, y:yCoord};
+        nodes.push(node);
+        var circle = $("<div></div>").addClass("node");
+        xCoord -= CIRCLE_WIDTH / 2;
+        yCoord -= CIRCLE_WIDTH / 2;
+        circle.css("left", Math.round(xCoord));
+        circle.css("top", Math.round(yCoord));
+        circle.html(nodes.length - 1);
+        $("#stage").append(circle);
+    }
+    
+    window.setInterval(function(){
+        
+    }, 100);
 });	
