c3e430e02ff453f20e2f7f34d4829327c7ed8550
Meera Ray <ronukray@gmail.com>
2019-07-07 14:31:45 -0700

    Implemented delete node function
    -changed styles a bit: changed hover and selection colors, made nodes and buttons more like buttons
    -limits on add node

--- scripts.js
+++ scripts.js
@@ -18,11 +18,11 @@ const DELETE_LINE = 3;
 const MOVE_NODE = 4;
 
 $(function() {
-	console.log("ready");
+//	console.log("ready");
     
     $(".control").click(function() {
         var index = $(".control").index($(this));
-        console.log(index);
+//        console.log(index);
         
         $(".control").removeClass("selected");
         $(this).addClass("selected");
@@ -37,15 +37,28 @@ $(function() {
         //console.log(e.pageX + ", " + e.pageY);
         //console.log(pos.left + ", " + pos.top);
         if(selected == NEW_NODE) {
-            handleAddNode(e.pageX - pos.left, e.pageY - pos.top)
+            handleAddNode(e.pageX - pos.left, e.pageY - pos.top);
         }
         else if (selected == DELETE_NODE) {
             
         }
     });
     
+    $("#stage").on("click", ".node", function(e) {
+        console.log("node clicked");
+        console.log($(this));
+        if(selected == DELETE_NODE) {
+            console.log("delete node clicked");
+            handleDeleteNode($(this));
+        }                 
+    });
+    
     function handleAddNode(xCoord, yCoord) {
-        console.log(xCoord, yCoord);
+//        console.log(xCoord, yCoord);
+        //check if inside limits
+        if(xCoord < CIRCLE_WIDTH / 2 || yCoord < CIRCLE_WIDTH / 2) {
+            return;
+        }
         var node = {x: xCoord, y:yCoord};
         nodes.push(node);
         var circle = $("<div></div>").addClass("node");
@@ -55,6 +68,13 @@ $(function() {
         circle.css("top", Math.round(yCoord));
         circle.html(nodes.length - 1);
         $("#stage").append(circle);
+    }
+
+    function handleDeleteNode(node) {
+        var index = $(".node").index(node);
+        console.log(index);
+        nodes.splice(index, 1);
+        node.remove()
     }
     
     window.setInterval(function(){
